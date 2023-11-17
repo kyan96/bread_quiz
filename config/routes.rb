eceef9d245ba042ef975f1d901d7fd85cf55a490
喜屋武 姫香 <142459207+kyan96@users.noreply.github.com>
2023-11-17 15:57:43 +0900

    トップページを作りました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
+
   get 'breads/show'
   get 'static_pages/top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
