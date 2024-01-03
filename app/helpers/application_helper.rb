90099d05903f7412a10ba908316d016af4acdd59
喜屋武 姫香 <142459207+kyan96@users.noreply.github.com>
2024-01-03 09:47:32 +0900

    問題がランダムで表示されるようにしました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,2 @@
 module ApplicationHelper 
-  def default_meta_tags
-    {
-      site: 'Bread_Quiz',
-      title: 'パンの名前当てクイズアプリ',
-      reverse: true,
-      separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
-      description: '沖縄県にあるパン屋さん『hoppepan』のパンの名前を答えるクイズアプリです！',
-      keywords: 'パン,クイズ,沖縄',   #キーワードを「,」区切りで設定する
-      canonical: request.original_url,   #優先するurlを指定する
-      noindex: ! Rails.env.production?,
-      og: {
-        site_name: 'Bread_Quiz',
-        title: 'パンの名前当てクイズアプリ',
-        description: '沖縄県にあるパン屋さん『hoppepan』のパンの名前を答えるクイズアプリです！', 
-        type: 'website',
-        url: request.original_url,
-        image: image_url('hoppe.png'),
-        locale: 'ja_JP'
-      },
-      twitter: {
-        card: 'summary_large_image',
-        site: '@hime_7r',
-        image: image_url('hoppe.png')
-      }
-    }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'questions/top'
-  get 'questions/show'
+  get '/questions/top', to: 'questions#top', as: 'top_questions'
+  get '/questions/show', to: 'questions#show', as: 'show_question'
+  post '/questions/check_answer', to: 'questions#check_answer', as: 'check_answer_question'
   root 'questions#top'
 end
